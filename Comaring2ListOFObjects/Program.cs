6721a5a4a3ce9ae0a27d99fe434ad660fd817be6
fardinosman <36036540+fardinosman@users.noreply.github.com>
2019-03-06 13:19:57 +0100

    working

--- Comaring2ListOFObjects/Program.cs
+++ Comaring2ListOFObjects/Program.cs
@@ -13,6 +13,45 @@ namespace Comaring2ListOFObjects
     /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+            Student s2 = new Student();
+
+            s1.Name = "hello";
+            s2.Name = "hello1";
+
+            if (s1 ==s2)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false ");
+            }
+            List<Student> list1 = new List<Student>();
+            list1.Add(s1);
+            list1.Add(s2);
+
+            Student s3 = new Student();
+            Student s4 = new Student();
+            s3.Name = "hello";
+            s4.Name = "hello1";
+
+            List<Student> list2 = new List<Student>();
+            list2.Add(s3);
+            list2.Add(s4);
+
+            if (list2.Contains(s1))
+            {
+                Console.WriteLine("true");
+            }
+            if (list2.SequenceEqual(list1))
+            {
+                Console.WriteLine(" checking sequence true");
+            }
+            if (list2.All(x=>list1.Contains(x)))
+            {
+                Console.WriteLine("list.all :  true");
+            }
         }
     }
 }
---
+++ Comaring2ListOFObjects/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Comaring2ListOFObjects
+{
+    public class Student :IEquatable<Student>
+    {
+        public string Name;
+        public int Age = 0;
+
+        public bool Equals(Student other)
+        {
+            return this.Name.Equals(other.Name) && this.Age.Equals(other.Age);
+        }
+        public static bool operator== (Student a, Student b)
+        {
+            return a.Name.Equals(b.Name) && a.Age.Equals(b.Age);
+        }
+        public static bool operator !=(Student a, Student b)
+        {
+            return !(a == b);
+        }
+    }
+}
